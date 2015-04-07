741fd4bf46117e65061af145bedf307b4a932b43
tan <tanmaykm@gmail.com>
2015-04-07 12:53:15 +0530

    remove Julia 0.4 deprecated syntax

--- src/JuliaBox.jl
+++ src/JuliaBox.jl
@@ -10,17 +10,14 @@ using Compat
 export APIResponder, register, process
 export APIInvoker, apicall, httpresponse, fnresponse, run_rest
 
-const ERR_CODES = Dict{Symbol, Array}([
-                        :success            => [200,  0, ""],
+const ERR_CODES = @compat Dict{Symbol, Array}(:success            => [200,  0, ""],
                         :invalid_api        => [404, -1, "invalid api"],
                         :api_exception      => [500, -2, "api exception"],
                         :invalid_data       => [500, -3, "invalid data"],
                         :dummy              => []
-                    ])
+                    )
 
-const CONTROL_CMDS = Dict{Symbol, Array}([
-                        :terminate => ["terminate"]
-                    ])
+const CONTROL_CMDS = @compat Dict{Symbol, Array}(:terminate => ["terminate"])
 if isless(Base.VERSION, v"0.4.0-")
     include("nullable.jl")
 end
--- test/srvr.jl
+++ test/srvr.jl
@@ -1,7 +1,8 @@
 using JuliaBox
+using Compat
 
 function testfn1(arg1, arg2; narg1=1, narg2=2)
-    return (int(arg1) * int(narg1)) + (int(arg2) * int(narg2))
+    return (@compat(Int(arg1)) * @compat(Int(narg1))) + (@compat(Int(arg2)) * @compat(Int(narg2)))
 end
 testfn2(arg1, arg2; narg1=1, narg2=2) = testfn1(arg1, arg2; narg1=narg1, narg2=narg2)
 
