d53dd11cb598ce39fc329b89ecdefde304625d8d
tan <tanmaykm@gmail.com>
2020-03-05 11:20:08 +0530

    do not use startup files in tests
    do not use startup files too in tests, for the same reasons as mentioned in #85

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,10 +6,11 @@ const inline_flag = opts.can_inline == 1 ? `` : `--inline=no`
 const cov_flag = (opts.code_coverage == 1) ? `--code-coverage=user` :
                  (opts.code_coverage == 2) ? `--code-coverage=all` :
                  ``
+const startup_flag = `--startup-file=no`
 
 function run_test(script, flags)
     srvrscript = joinpath(dirname(@__FILE__), script)
-    srvrcmd = `$(joinpath(Sys.BINDIR, "julia")) $cov_flag $inline_flag $script $flags`
+    srvrcmd = `$(joinpath(Sys.BINDIR, "julia")) $startup_flag $cov_flag $inline_flag $script $flags`
     println("Running tests from ", script, "\n", "="^60)
     ret = withenv("JULIA_DEPOT_PATH"=>join(DEPOT_PATH, Sys.iswindows() ? ';' : ':')) do
         run(srvrcmd)
--- test/test_clntsrvr.jl
+++ test/test_clntsrvr.jl
@@ -8,10 +8,11 @@ const inline_flag = opts.can_inline == 1 ? `` : `--inline=no`
 const cov_flag = (opts.code_coverage == 1) ? `--code-coverage=user` :
                  (opts.code_coverage == 2) ? `--code-coverage=all` :
                  ``
+const startup_flag = `--startup-file=no`
 
 function spawn_srvr()
     srvrscript = joinpath(dirname(@__FILE__), "srvr.jl")
-    srvrcmd = `$(joinpath(Sys.BINDIR, "julia")) $cov_flag $inline_flag $srvrscript --runsrvr`
+    srvrcmd = `$(joinpath(Sys.BINDIR, "julia")) $startup_flag $cov_flag $inline_flag $srvrscript --runsrvr`
     println("spawining $srvrcmd")
     srvrproc = withenv("JULIA_DEPOT_PATH"=>join(DEPOT_PATH, Sys.iswindows() ? ';' : ':')) do
         run(srvrcmd, wait=false)
